f165689914d131a0d7b5e430d063cd2c7a1e09cf
hopedeveloper08 <hopedeveloper08@gmail.com>
2024-03-18 02:06:28 +0330

    set ichimoku requirements

---
+++ models/ichimoku.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+
+def tenkansen():
+    pass
+
+
+def kijunsen():
+    pass
+
+
+def chiku_span():
+    pass
+
+
+def senkou_A():
+    pass
+
+
+def senkou_B():
+    pass
+
+
+def is_buy():
+    pass
+
+
+def is_sell():
+    pass
+
+
+def trend_status():
+    pass
+
+
+def next_step_to_buy():
+    pass
