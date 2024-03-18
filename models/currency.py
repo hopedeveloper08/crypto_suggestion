7bcb76f760ee52921ed3b0c5a62e433b7eafd499
hopedeveloper08 <hopedeveloper08@gmail.com>
2024-03-18 15:44:23 +0330

    fetch_currency_data method impeliments

--- models/currency.py
+++ models/currency.py
@@ -1,19 +1,37 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 import requests
+from datetime import datetime
 
 
-CURRENCY_DATA = None
+CURRENCIES_SUPPORTED = [
+    "bitcoin",
+    'ethereum',
+    'binancecoin'
+    'solana',
+    'ripple'
+    'cardano',
+    'tron'
+    'dogecoin'
+    'shiba'
+    'polkadot'
+]
 
 
-def get_currency_data(symbol='BTC', time_frame='D'):
-    pass
+def fetch_currency_data(name='bitcoin'):
+    url = f"https://api.coingecko.com/api/v3/coins/{name}/market_chart"
+    params = {
+        "vs_currency": "usd",
+        "days": "60",
+        "interval": "daily",
+    }
+    response = requests.get(url, params=params)
 
-
-def data_not_exist():
-    if CURRENCY_DATA is None:
-        return True
-
-    return False
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        raise Exception(f"Error fetching data from {name}")
 
 
 def draw_chart():
