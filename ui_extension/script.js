7d49b96861c19d8fc81f25b2f2870e722beed105
hopedeveloper08 <hopedeveloper08@gmail.com>
2024-03-19 06:27:54 +0330

    add suggest section in search

--- ui_extension/script.js
+++ ui_extension/script.js
@@ -5,3 +5,69 @@ function navigateToHomePage() {
 function navigateToSuggestPage() {
     window.location.href = 'suggest.html';
 }
+
+const searchInput = document.getElementById('search-input');
+const suggestionsDiv = document.getElementById('suggestions');
+
+const suggestions = [
+    'bitcoin',
+    'ethereum',
+    'binancecoin',
+    'solana',
+    'ripple',
+    'cardano',
+    'tron',
+    'dogecoin',
+    'shiba',
+    'polkadot',
+];
+
+const logos = {
+    bitcoin: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1.png',
+    ethereum: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png',
+    binancecoin: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png',
+    solana: 'https://s2.coinmarketcap.com/static/img/coins/64x64/5426.png',
+    ripple: 'https://s2.coinmarketcap.com/static/img/coins/64x64/52.png',
+    cardano: 'https://s2.coinmarketcap.com/static/img/coins/64x64/2010.png',
+    tron: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1958.png',
+    dogecoin: 'https://s2.coinmarketcap.com/static/img/coins/64x64/74.png',
+    shiba: 'https://s2.coinmarketcap.com/static/img/coins/64x64/5994.png',
+    polkadot: 'https://s2.coinmarketcap.com/static/img/coins/64x64/6636.png'
+}
+
+searchInput.addEventListener('focus', function() {
+    showSuggestions();
+});
+
+function showSuggestions() {
+        suggestionsDiv.innerHTML = '';
+        suggestions.forEach(function(suggestion) {
+
+            const suggestionElement = document.createElement('button');
+            suggestionElement.className = 'list-group-item list-group-item-action';
+            suggestionElement.style.backgroundColor = "rgb(50, 53, 70)";
+            suggestionElement.addEventListener('click', function() {
+                searchInput.value = suggestion;
+                hideSuggestions();
+            });
+
+            const logoElement = document.createElement('img');
+            logoElement.src = logos[suggestion]
+            logoElement.style.width = "32px";
+            logoElement.style.height= "32px";
+            suggestionElement.appendChild(logoElement)
+
+            const text = document.createElement('span');
+            text.textContent = suggestion;
+            text.className = "h5 ms-3"
+            text.style.color = "rgb(151, 156, 176)"
+            suggestionElement.appendChild(text)
+
+            suggestionsDiv.appendChild(suggestionElement);
+        });
+        suggestionsDiv.style.display = 'block';
+    }
+
+function hideSuggestions() {
+    suggestionsDiv.style.display = 'none';
+}
