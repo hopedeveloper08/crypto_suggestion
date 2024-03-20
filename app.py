e4903f52f4d8e6751bdfc48c25f169902937d3ca
hopedeveloper08 <hopedeveloper08@gmail.com>
2024-03-20 04:18:45 +0330

    api currency status on backend

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from models.currency import fetch_currency_data, create_dataframe, CURRENCIES_SUPPORTED
 from threading import Thread
-
+from models.ichimoku import signal_buy, signal_sell, trend_status, steps_to_buy
 
 CURRENCY_DATA = dict()
 
@@ -23,6 +23,17 @@ CORS(app)
 @app.route("/currencies_supported", methods=["GET"])
 def currencies_supported():
     return {"data": CURRENCIES_SUPPORTED}
+
+
+@app.route("/get_currency_status", methods=["GET"])
+def get_currency_status(currency):
+    data = CURRENCY_DATA[currency]
+    return {
+        "signal_buy": signal_buy(data),
+        "signal_sell": signal_sell(data),
+        "trend_status": trend_status(data),
+        "steps_to_buy": steps_to_buy(data),
+    }
 
 
 if __name__ == '__main__':
