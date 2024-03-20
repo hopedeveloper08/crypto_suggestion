409c149f039216b8246703732d66159e5bfa6914
hopedeveloper08 <hopedeveloper08@gmail.com>
2024-03-21 02:53:50 +0330

    add show chart

--- ui_extension/script_suggest.js
+++ ui_extension/script_suggest.js
@@ -1,10 +1,20 @@
 document.getElementById("home-btn").addEventListener("click", function() {window.location.href = 'popup.html';});
 document.getElementById("currency-name").innerText = localStorage.getItem("currency");
 
-fetch(`http://localhost:5000/get_currency_logo/${localStorage.getItem("currency")}`)
+const currency = localStorage.getItem("currency");
+
+fetch(`http://localhost:5000/get_currency_logo/${currency}`)
     .then(response => response.json())
     .then((data) => {
         if ('logo' in data)
             document.getElementById("currency-logo").setAttribute("src", data.logo);
         })
     .catch(() => document.getElementById("currency-logo").style.display = "none");
+
+fetch(`http://localhost:5000/draw_chart/${currency}`)
+    .then(response => {
+        if (response.status === 200) {
+            document.getElementById("chart-img").setAttribute("src", "image/chart.png")
+        }
+    })
+    .catch(e => console.log(e))
