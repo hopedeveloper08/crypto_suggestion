f4e2fe20ad86674856455ab545822c85b4975c15
hopedeveloper08 <hopedeveloper08@gmail.com>
2024-03-21 05:42:46 +0330

    add show signal result

--- ui_extension/script_suggest.js
+++ ui_extension/script_suggest.js
@@ -18,3 +18,23 @@ fetch(`http://localhost:5000/draw_chart/${currency}`)
         }
     })
     .catch(e => console.log(e))
+
+fetch(`http://localhost:5000/get_currency_status/${currency}`)
+    .then(response => response.json())
+    .then(data => {
+        setResult(data);
+
+    })
+    .catch(e => console.log(e))
+
+
+function setResult(data) {
+    const result = document.getElementById("result")
+    console.log(data.signal_buy)
+    if (data.signal_buy)
+        result.innerHTML = '<span class="badge text-bg-success"><strong>BUY</strong></span>';
+    else if (data.signal_sell)
+        result.innerHTML = '<span class="badge text-bg-danger"><strong>SELL</strong></span>';
+    else
+        result.innerHTML = '<span class="badge text-bg-warning"><strong>HOLD</strong></span>';
+}
