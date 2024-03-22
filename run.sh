7389d74b354acbae80a9a2ef1f6d8ebab4e9e6d9
hopedeveloper08 <hopedeveloper08@gmail.com>
2024-03-22 20:24:38 +0330

    update bash script

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
-source .venv/bin/activate
+#!/bin/bash
+
+if [ -d ".venv" ]; then
+    source .venv/bin/activate
+else
+    echo "OK"
+    python3 -m virtualenv ".venv"
+    source .venv/bin/activate
+    python3 -m pip install -r requirements.txt
+fi
+
 
 python3 app.py
